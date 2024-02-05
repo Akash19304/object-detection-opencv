eba77d9b4b9e3a9ac769eba93302760a7ab0393d
Akash <123761006+Akash19304@users.noreply.github.com>
2024-02-05 22:04:08 +0530

    Added files

---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+from utils import get_limits
+from PIL import Image
+
+
+yellow = [255, 0, 0] ## yellow in BGR
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # convert it to hsv format
+
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1,y1), (x2,y2), color=(0,255,0), thickness=5)
+
+    # print(bbox)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
